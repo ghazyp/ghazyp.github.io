5ba454c50589fbe2815b92015944275cc604d0f1
ghazyp <56493006+ghazyp@users.noreply.github.com>
2024-11-10 17:32:30 -0500

    Update apples.rb

--- Falling Apples/apples.rb
+++ Falling Apples/apples.rb
@@ -1,4 +1,4 @@
-def read_input(file)
+  def read_input(file)
     file = File.open('apples.in', 'r')
     rows, cols = file.readline.split.map(&:to_i)
     grid = []
@@ -7,38 +7,38 @@ def read_input(file)
     end
     file.close
     return grid, rows, cols
-end
+  end
   
-def simulate_falling_apples(grid, rows, cols)
+  def simulate_falling_apples(grid, rows, cols)
     cols.times do |col|
-        # Start from the bottom and move upwards
-        empty_row = rows - 1
-        (rows - 1).downto(0) do |row|
-            if grid[row][col] == 'a'
-                if row != empty_row
-                    grid[empty_row][col], grid[row][col] = grid[row][col], '.'
-                end
-                empty_row -= 1
-            elsif grid[row][col] == '#'
-                empty_row = row - 1
-            end
-        end    
+      # Start from the bottom and move upwards
+      empty_row = rows - 1
+      (rows - 1).downto(0) do |row|
+        if grid[row][col] == 'a'
+          if row != empty_row
+            grid[empty_row][col], grid[row][col] = grid[row][col], '.'
+          end
+          empty_row -= 1
+        elsif grid[row][col] == '#'
+          empty_row = row - 1
+        end
+      end    
     end
     grid
-end
-
-def print_grid(grid)
-    grid.each do |row|
-        puts row.join('')
-    end
-end
+  end
   
-def main()
-    file = 'apple.in'
+  def print_grid(grid)
+    grid.each do |row|
+      puts row.join('')
+    end
+  end
+  
+  def main
+    file = 'apples.in'
     grid, rows, cols = read_input(file)
-    grid = simulate_falling_apples(grid, rows, cols) 
+    grid = simulate_falling_apples(grid, rows, cols)
     print_grid(grid)
-end
-
-main
+  end
+  
+  main
   
